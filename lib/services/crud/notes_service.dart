dc29fe180676f62e4ae95abbded30f37a2c942c4
Mustafa Enes Cunedioğlu <enescnd1152@gmail.com>
2023-06-02 21:58:53 +0300

    creating database pt2

--- lib/services/crud/notes_service.dart
+++ lib/services/crud/notes_service.dart
@@ -24,8 +24,17 @@ class NoteService {
 	"email"	TEXT NOT NULL UNIQUE,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );''';
-
       await db.execute(createUserTable);
+      const createNoteTable = '''
+CREATE TABLE IF NOT EXISTS "note" (
+	"id"	INTEGER NOT NULL,
+	"user_id"	INTEGER NOT NULL,
+	"text"	TEXT,
+	"is_synced_with_cloud"	INTEGER DEFAULT 0,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+''';
+      await db.execute(createNoteTable);
     } on MissingPlatformDirectoryException {
       throw UnableToGetDocumentsDirectory();
     }
