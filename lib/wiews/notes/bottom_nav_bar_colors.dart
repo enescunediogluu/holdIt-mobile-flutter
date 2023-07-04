2a98f44dbc17547e5d768adb359341173d8bc031
Mustafa Enes Cunedioğlu <enescnd1152@gmail.com>
2023-07-04 21:18:51 +0300

    AppBar ui is finished

---
+++ lib/wiews/notes/bottom_nav_bar_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const navBarBgColor = Color(0xff654E92);
+const navBarTabActiveColor = Colors.white;
+var todo_icon_color = navBarTabActiveColor;
+var home_icon_color = navBarBgColor;
+var todo_background_color = navBarBgColor;
+var home_background_color = navBarTabActiveColor;
--- lib/wiews/notes/notes_view.dart
+++ lib/wiews/notes/notes_view.dart
@@ -8,6 +8,7 @@ import 'package:holdit/services/auth/bloc/auth_event.dart';
 import 'package:holdit/services/cloud/cloud_note.dart';
 import 'package:holdit/services/cloud/firebase_cloud_storage.dart';
 import 'package:holdit/utilities/dialogs/logout_dialog.dart';
+import 'package:holdit/wiews/notes/bottom_nav_bar_colors.dart';
 import 'package:holdit/wiews/notes/notes_list_view.dart';
 
 class NoteView extends StatefulWidget {
@@ -31,107 +32,163 @@ class _NoteViewState extends State<NoteView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xffFDF4F5),
-        appBar: AppBar(
-          title: const Text(
-            'Your Notes',
-            style: TextStyle(
-                color: Color(0XFF000000),
-                fontFamily: 'MainFont',
-                fontSize: 30,
-                fontWeight: FontWeight.bold),
+      backgroundColor: const Color(0xffFDF4F5),
+      appBar: AppBar(
+        title: const Text(
+          'Your Notes',
+          style: TextStyle(
+              color: Color(0XFF000000),
+              fontFamily: 'MainFont',
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 100,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
+            },
+            icon: const Icon(Icons.add, color: Colors.black, size: 35),
           ),
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          toolbarHeight: 100,
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
-              },
-              icon: const Icon(Icons.add, color: Colors.black, size: 35),
+          PopupMenuButton<MenuAction>(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20.0),
+              ),
             ),
-            PopupMenuButton<MenuAction>(
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20.0),
+            elevation: 3,
+            surfaceTintColor: Colors.black,
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+              size: 35,
+            ),
+            color: const Color(0xffFDF4F5),
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    // ignore: use_build_context_synchronously
+                    context.read<AuthBloc>().add(
+                          const AuthEventLogOut(),
+                        );
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.logout,
+                        color: Colors.black,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        'Log out',
+                        style: TextStyle(
+                            fontFamily: 'MainFont',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 19),
+                      ),
+                    ],
+                  ),
+                ),
+              ];
+            },
+          )
+        ],
+      ),
+      body: StreamBuilder(
+        stream: _noteService.allNotes(ownerUserId: userId),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              if (snapshot.hasData) {
+                final allNotes = snapshot.data as Iterable<CloudNote>;
+                return NotesListView(
+                  notes: allNotes,
+                  onDeleteNote: (note) async {
+                    await _noteService.deleteNote(documentId: note.documentId);
+                  },
+                  onTap: (note) {
+                    Navigator.of(context)
+                        .pushNamed(createOrUpdateNoteRoute, arguments: note);
+                  },
+                );
+              } else {
+                return const CircularProgressIndicator();
+              }
+
+            default:
+              return const CircularProgressIndicator();
+          }
+        },
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: navBarBgColor,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: home_background_color,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.home,
+                      color: home_icon_color,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        todo_icon_color = navBarTabActiveColor;
+                        todo_background_color = navBarBgColor;
+                        home_background_color = navBarTabActiveColor;
+                        home_icon_color = navBarBgColor;
+                      });
+                    },
+                  ),
                 ),
               ),
-              elevation: 3,
-              surfaceTintColor: Colors.black,
-              icon: const Icon(
-                Icons.menu,
-                color: Colors.black,
-                size: 35,
-              ),
-              color: const Color(0xffFDF4F5),
-              onSelected: (value) async {
-                switch (value) {
-                  case MenuAction.logout:
-                    final shouldLogout = await showLogOutDialog(context);
-                    if (shouldLogout) {
-                      // ignore: use_build_context_synchronously
-                      context.read<AuthBloc>().add(
-                            const AuthEventLogOut(),
-                          );
-                    }
-                }
-              },
-              itemBuilder: (context) {
-                return const [
-                  PopupMenuItem<MenuAction>(
-                    value: MenuAction.logout,
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.logout,
-                          color: Colors.black,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          'Log out',
-                          style: TextStyle(
-                              fontFamily: 'MainFont',
-                              fontWeight: FontWeight.bold,
-                              fontSize: 19),
-                        ),
-                      ],
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: todo_background_color,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: IconButton(
+                    icon: Image.asset(
+                      'lib/images/todo_icon.png',
+                      color: todo_icon_color,
                     ),
+                    onPressed: () {
+                      setState(() {
+                        home_icon_color = navBarTabActiveColor;
+                        home_background_color = navBarBgColor;
+                        todo_icon_color = navBarBgColor;
+                        todo_background_color = navBarTabActiveColor;
+                      });
+                    },
                   ),
-                ];
-              },
-            )
-          ],
+                ),
+              )
+            ],
+          ),
         ),
-        body: StreamBuilder(
-          stream: _noteService.allNotes(ownerUserId: userId),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-              case ConnectionState.active:
-                if (snapshot.hasData) {
-                  final allNotes = snapshot.data as Iterable<CloudNote>;
-                  return NotesListView(
-                    notes: allNotes,
-                    onDeleteNote: (note) async {
-                      await _noteService.deleteNote(
-                          documentId: note.documentId);
-                    },
-                    onTap: (note) {
-                      Navigator.of(context)
-                          .pushNamed(createOrUpdateNoteRoute, arguments: note);
-                    },
-                  );
-                } else {
-                  return const CircularProgressIndicator();
-                }
-
-              default:
-                return const CircularProgressIndicator();
-            }
-          },
-        ));
+      ),
+    );
   }
 }
